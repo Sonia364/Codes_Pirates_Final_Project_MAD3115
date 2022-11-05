7f078748da97ca60c7dd79e0655fe9379f8b1664
Sonia364 <c0872364@mylambton.ca>
2022-11-05 16:53:52 -0400

    MAD Final Project - implemented functionality for employee type selection

--- Codes_Pirates_Final_Project_MAD3115/RegisterVC.swift
+++ Codes_Pirates_Final_Project_MAD3115/RegisterVC.swift
@@ -7,23 +7,88 @@
 
 import UIKit
 
-class RegisterVC: UIViewController {
-
+class RegisterVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    
+    @IBOutlet weak var vehicleType: UISegmentedControl!
+    @IBOutlet weak var vehicleColorPicker: UIPickerView!
+    @IBOutlet weak var employeeTypePicker: UIPickerView!
+    @IBOutlet weak var employeeSpecInput: UITextField!
+    @IBOutlet weak var employeeSpecLabel: UILabel!
+    
+    var employeeTypeData: [String] = []
+    var vehicleColorData:  [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       employeeTypeData = ["Manager","Programmer","Tester"]
+       vehicleColorData = ["Red", "Blue", "Yellow", "Green", "Orange", "Purple", "Pink", "Brown", "White", "Black", "Beige"]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1{
+            return employeeTypeData.count
+        }else{
+            return vehicleColorData.count
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1{
+            return employeeTypeData[row]
+        }else{
+            return vehicleColorData[row]
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)  {
+        if pickerView.tag == 1{
+            let selectedValue = employeeTypeData[row]
+            employeeSpecLabel.isHidden = false
+            employeeSpecInput.isHidden = false
+            switch selectedValue{
+                case "Manager":
+                    employeeSpecLabel.text = "# Clients"
+                case "Tester":
+                    employeeSpecLabel.text = "# Bugs"
+                case "Programmer":
+                    employeeSpecLabel.text = "# Projects"
+                default:
+                    employeeSpecLabel.text = "# Clients"
+                    
+            }
+            
+        }else{
+            
+        }
+       
+    }
+    
+    @IBAction func vehicleTypeSelection(_ sender: Any) {
+        //vehicleType.selectedSegmentTintColor = .blue
+        switch vehicleType.selectedSegmentIndex {
+                case 0:
+                   // some code
+            vehicleType.selectedSegmentTintColor = .white
+            vehicleType.backgroundColor = .gray
+                case 1 :
+                  // some code
+            vehicleType.backgroundColor = .gray
+                default:
+                    break
+                }
+                
+    }
+    
+    
+
 
 }
+
