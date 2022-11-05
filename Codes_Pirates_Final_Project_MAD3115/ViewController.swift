efd024284ab8f82664e9e4422b229689a3981c46
Sonia364 <c0872364@mylambton.ca>
2022-11-05 15:45:58 -0400

    Initial Commit

---
+++ Codes_Pirates_Final_Project_MAD3115/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codes_Pirates_Final_Project_MAD3115
+//
+//  Created by Others on 2022-11-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
