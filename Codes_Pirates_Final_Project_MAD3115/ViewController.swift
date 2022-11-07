2d23bef2d5eed11b292a951ed5f5a7c840d3b79b
AdityaRanjan466 <c0880466@mylambton.ca>
2022-11-07 10:30:54 -0500

    MAD Final Project -  Added search bar to employee list

--- Codes_Pirates_Final_Project_MAD3115/ViewController.swift
+++ Codes_Pirates_Final_Project_MAD3115/ViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var regUsers = [RegisteredUser]()
     
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchdata =  [RegisteredUser]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let newRegUser =  RegisteredUser(firstName: "Serge", lastName: "Roy", birthYear: "1976", monthlySalary: "5000", occupationRate: "100", employeeId:"12", employeeType: "Manager", employeeSpecNumber: "18", vehicleType:  "Car", vehicleCarType: "Sports", vehicleSideCar: "Yes", vehicleModel:"Honda", plateNumber: "12345", vehicleColor: "White")
-
-        //if employeeExists.isEmpty {
-         RegisteredUser.addEmployee(newEmployee: newRegUser)
         
+        let newRegUser =  RegisteredUser(firstName: "Serge", lastName: "Roy", birthYear: "1976", monthlySalary: "5000", occupationRate: "100", employeeId:"12", employeeType: "Manager", employeeSpecNumber: "18", vehicleType:  "Car", vehicleCarType: "Sports", vehicleSideCar: "Yes", vehicleModel:"Honda", plateNumber: "12345", vehicleColor: "White")
+        
+        //if employeeExists.isEmpty {
+        RegisteredUser.addEmployee(newEmployee: newRegUser)
+        searchdata = RegisteredUser.regUserList
         print(RegisteredUser.regUserList)
         //tableView.reloadData()
         // Do any additional setup after loading the view.
@@ -32,20 +37,18 @@ class ViewController: UIViewController {
         
     }
     
-    
-
 }
-extension ViewController : UITableViewDelegate, UITableViewDataSource
+extension ViewController : UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate
 {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       // return employeeData.count
-        return RegisteredUser.regUserList.count
+        // return employeeData.count
+        return searchdata.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "")
-        let rowData = RegisteredUser.regUserList[indexPath.row]
+        let rowData = searchdata[indexPath.row]
         let name = String(rowData.firstName) + " " + String(rowData.lastName)
         let id  = String(rowData.employeeId)
         cell.textLabel?.text = "Name: \(name)"
@@ -58,12 +61,12 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource
         let EmployeeDetailsVC = mainSB.instantiateViewController(withIdentifier: "EmployeeDetailsScene") as? EmployeeDetailsVC
         EmployeeDetailsVC?.delegate = self
         
-        EmployeeDetailsVC?.selectedEmployee = RegisteredUser.regUserList[indexPath.row]
+        EmployeeDetailsVC?.selectedEmployee = searchdata[indexPath.row]
         if let EmployeeDetailsVC = EmployeeDetailsVC {
             navigationController?.pushViewController(EmployeeDetailsVC, animated: true)
         }
     }
     
-    
+
 }
 
